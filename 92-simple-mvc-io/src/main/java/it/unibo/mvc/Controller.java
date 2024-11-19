5f660bbc02d997ab71bcde96336cdd450ed922d6
emme004 <andrea.monti24@studio.unibo.it>
2024-11-19 16:12:09 +0100

    first part of 92

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,64 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    public static final String SEP = File.separator;
+    public static final String FILE_NAME = System.getProperty("user.home") + SEP + "output.txt";
+    private File destination;
+
+    public Controller(){
+        this.destination = new File(FILE_NAME);
+    }
+
+    /**
+     * A method for setting a File as a current file
+     * @return setting file
+     */
+    public void setDestination(final File dfile){
+        setDestination(dfile);
+    }
+
+    /**
+     * A method for getting the current File
+     * @return the current file
+     */
+    public File getDestination() {
+        return destination;
+    }
+
+    /**
+     * A method for getting the path (in form of String) 
+     * of the current `File`
+     * @return the current file path
+     */
+    public String getPathDestination(){
+        return destination.getPath();
+    }
+
+    /**
+     * A method that gets a `String` as input and 
+     * saves its content on the current file.
+     * @throws IOException
+     */
+    public void save (final String adder) throws IOException{
+        try(PrintStream ps = new PrintStream(destination)){
+    
+        }
+
+    }
+
+    
+    
+
+
+
+
 
 }
