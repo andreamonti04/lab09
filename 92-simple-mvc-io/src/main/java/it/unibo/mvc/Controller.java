2d2669f9c6457516f4a5f3148f2e7a6b7f47c31f
emme004 <andrea.monti24@studio.unibo.it>
2024-11-20 20:00:06 +0100

    Built ok

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -9,24 +9,29 @@ import java.nio.charset.StandardCharsets;
  * Application controller. Performs the I/O.
  */
 public class Controller {
-    public static final String SEP = File.separator;
-    public static final String FILE_NAME = System.getProperty("user.home") + SEP + "output.txt";
+
+    private static final String SEP = File.separator;
+    private static final String FILE_NAME = System.getProperty("user.home") + SEP + "output.txt";
     private File currentFile;
 
-    public Controller(){
+    /**
+     * Constructor of Controller.
+     * Set a @param FILE_NAME for the initial name of current file
+     */
+    public Controller() {
         this.currentFile = new File(FILE_NAME);
     }
 
     /**
-     * Sets a File as a current file
-     * @return setting file
+     * Sets a File as a current file.
+     * @param dfile
      */
-    public void setCurrentFile(final File dfile){
+    public void setCurrentFile(final File dfile) {
         this.currentFile = dfile;
     }
 
     /**
-     * Gets the current File
+     * Gets the current File.
      * @return the current file
      */
     public File getcurrentFile() {
@@ -34,34 +39,25 @@ public class Controller {
     }
 
     /**
-     * Gets the path (in form of String) 
-     * of the current `File`
+     * Gets the path (in form of String) of the current `File`.
      * @return the current file path
      */
-    public String getPathDestination(){
+    public String getPathDestination() {
         return currentFile.getPath();
     }
 
     /**
-     * Gets a `String` as input and 
-     * saves its content on the current file.
+     * Gets a `String` as input and saves its content on the current file.
      * @throws IOException
+     * @param adder
      */
-    public void save (final String adder) throws IOException{
-        try(PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)){
+    public void save(final String adder) throws IOException {
+        try (PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
             if (adder.isEmpty()) {
                 ps.close();
                 throw new IllegalArgumentException("The string is empty");
-            }    
-            ps.println(adder);      
+            }
+            ps.println(adder);
         }
     }
-
-    
-    
-
-
-
-
-
 }
