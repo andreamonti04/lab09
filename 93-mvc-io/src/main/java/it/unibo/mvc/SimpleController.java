5b5dc2f7e008c9bc62ace2eb6e91eaf78cbf5c67
emme004 <andrea.monti24@studio.unibo.it>
2024-11-20 20:24:33 +0100

    unimplements methods written

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void setString(String stringToPrint) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setString'");
+    }
+
+    @Override
+    public String getNextString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNextString'");
+    }
+
+    @Override
+    public List<String> getHistory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHistory'");
+    }
+
+    @Override
+    public void getCurrentString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentString'");
+    }
 
 }
